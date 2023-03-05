3185b6454fce3affa15d2406c905d99b78aa69db
kirans1136 <comfreshers@gmail.com>
2023-03-05 16:15:25 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Kia from './NavBars/Kia';
 import Ford from './NavBars/Ford';
-
+import Jaguar from "./NavBars/Jaguar";
 
 const App=() =>{
     return (
         <div>
          <Kia/>
          <Ford/>
+         <Jaguar/>
         </div>
     )
 }
