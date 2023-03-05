8a2c391a56a49f233b657dc6c287fc2a13504130
kirans1136 <comfreshers@gmail.com>
2023-03-05 19:10:19 +0530

    5Tata

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import Kia from './NavBars/Kia';
 import Ford from './NavBars/Ford';
 import Jaguar from "./NavBars/Jaguar";
+import SkoDa from './NavBars/SkoDa';
+import Tata from './NavBars/Tata';
+
+
+
 
 const App=() =>{
     return (
@@ -9,6 +14,9 @@ const App=() =>{
          <Kia/>
          <Ford/>
          <Jaguar/>
+        <SkoDa/>
+        <Tata/>
+         
         </div>
     )
 }
