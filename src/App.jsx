70bb2056f767b9ea556d9cceacb27feb1efec630
KIRAN <120558773+kirans1136@users.noreply.github.com>
2023-03-08 17:36:40 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+<<<<<<< HEAD
+import React from "react";
+import Img from "./images.jpeg"
+
+const App =()=>{
+    return(
+        <>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3STpHoQ2-Uq34usz_4--FbOVvbsgGy9iCEQ&usqp=CAU" alt="" />
+        <img src={Img} alt="" />
+        </>
+    )
+}
+=======
 import React from "react";
 import { Component } from "react";
 import Cbc from './Props/Cbc';
@@ -10,4 +23,5 @@ class App extends Component{
         )
     }
 }
+>>>>>>> 20c447bd892be069b4eccf44e79842410bb2a067
 export default App
---
+++ src/Nav.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Nav =(props)=>{
+    return(
+      <h1>
+        {props.number} {props.bollean}{ props.data}
+      </h1>
+    )
+}
+export default Nav
